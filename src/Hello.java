32332f335c85a2af3d18b43ee259d51c9affb727
CaliBitcoins <coinsector@gmail.com>
2019-10-09 08:57:05 -0700

    Udemy HelloWorld Excercise and Notes

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+// Class signature components:
+// 1) public - access modifier keyword
+// 2) class
+// 3) class name
+public class Hello {
+
+    // public is an access modifier
+    // static  is a keyword that requires understanding of additional concepts
+    // void is the return type - indicates method will not return anything
+    public static void main(String[] args) {
+        // Statement - a complete command to be executed can include one or more expressions
+        System.out.println("Hello World");
+    }
+}
